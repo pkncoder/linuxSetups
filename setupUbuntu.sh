68be6b38e31afecaaf96e39049231403fa672c11
pkncoder <alex.h.preston@gmail.com>
2025-09-20 10:34:37 -0400

    Hopefully fixed ubunut

--- setupUbuntu.sh
+++ setupUbuntu.sh
@@ -9,15 +9,15 @@ sudo apt install flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 echo "Installing plasma and sddm"
-sudo apt sddm plasma-desktop
+sudo apt install sddm plasma-desktop
 
 echo "Adding sddm to systemctl"
-systemctl enable sddm.service
+sudo systemctl enable sddm.service
 
 mkdir -p /tmp/host_files
 # echo "share /tmp/host_files 9p trans=virtio,rw,nofail 0 0" >> /etc/fstab
 
 echo "Rebooting in 10 seconds"
 sleep 10
-reboot now
+sudo reboot now
 
