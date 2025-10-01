633582f2ba4b1ef35ce94e34aed8d41545ba114f
pkncoder <alex.h.preston@gmail.com>
2025-10-01 16:21:57 -0400

    Updated for a more full-setup

--- setupArch.sh
+++ setupArch.sh
@@ -1,36 +1,46 @@
-# echo "Installing sudo and configuring sudoers"
-# pacman -Syu sudo
-
-# export EDITOR="nvim"
-# sudo visudo
-
-echo "Installing terminal emulator, app launcher, file explorer, and firefox"
-sudo pacman -Syu foot dolphin firefox
+echo "Installing terminal emulator, app launcher, file explorer, firefox, image viewer, and decompresser"
+sudo pacman -Syu foot wofi nautilus firefox mirage ark
 
 echo "Installing fastfetch git, and base-devel"
-sudo pacman -S fastfetch git base-devel
+sudo pacman -S fastfetch base-devel stow
 
 echo "Installing flatpak"
 sudo pacman -S flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
-echo "Installing yay, must be in user"
+echo "Installing yay"
 git clone "https://aur.archlinux.org/yay.git"
 cd yay
 makepkg -si
 cd ../
 sudo rm -rf ./yay
 
-echo "Installing plasma and sddm"
-pacman -S sddm plasma-desktop plasma-meta
+echo "Installing sddm and waybar"
+sudo pacman -S sddm waybar
+
+echo "Installing hyprland, hyprland utils."
+sudo pacman -S hyprland hyprlock hyprpaper
+sudo pacman -S otf-font-awesome ttf-cascadia-code-nerd
+
+echo "Cloning dotfiles"
+git clone "https://github.com/typecraft-dev/dotfiles"
+cd dotfiles
+mkdir -p ~/.config/hypr
+
+echo "Stowing dotfiles"
+stow hyprlock/
+stow wofi/
+stow hyprpaper/
+stow waybar/
+stow backgrounds/
+stow hyprmocha/
 
 echo "Adding sddm to systemctl"
-systemctl enable sddm.service
+sudo systemctl enable sddm.service
 
-mkdir /tmp/host_files
+# mkdir /tmp/host_files
 # echo "share /tmp/host_files 9p trans=virtio,rw,nofail 0 0" >> /etc/fstab
 
 echo "Rebooting in 10 seconds"
 sleep 10
-reboot now
-
+sudo reboot now
